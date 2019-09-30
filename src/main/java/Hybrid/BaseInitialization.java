15336704765903c035a6c2b95e76a82830f7a638
ramme1023 <rameshramme4393@gmail.com>
2019-09-30 21:05:06 +0530

    added screenshot function and started working on new brach

--- src/main/java/Hybrid/BaseInitialization.java
+++ src/main/java/Hybrid/BaseInitialization.java
@@ -3,8 +3,13 @@
  */
 package Hybrid;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -39,6 +44,11 @@ public class BaseInitialization {
 		}
 		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 		return driver;	
+	}
+	
+	public void Screenshot(String str) throws IOException {
+		File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(src, new File(""));
 	}
 	
 
--- src/test/java/Hybrid/TesterLogin.java
+++ src/test/java/Hybrid/TesterLogin.java
@@ -8,10 +8,13 @@ public class TesterLogin extends BaseInitialization{
 	@BeforeTest
 	public void intialize() {
 		driver = InitializeDriver();
+		String className = this.getClass().getSimpleName();
+		System.out.println(this.getClass().getSimpleName());
 	}
 	@Test
 	public void OpenUrl() {
 		driver.get("https://www.ultimatix.net");
+		
 	}
 
 }

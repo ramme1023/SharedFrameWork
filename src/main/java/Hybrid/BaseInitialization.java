5c04ba2a0023809217b84a74217632c7b6692e36
ramme1023 <rameshramme4393@gmail.com>
2019-10-01 22:44:53 +0530

    Added WordDocument creation function, resolved manven dependencies issues and changes few path names in BaseClass

--- src/main/java/Hybrid/BaseInitialization.java
+++ src/main/java/Hybrid/BaseInitialization.java
@@ -5,13 +5,19 @@ package Hybrid;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.poi.util.Units;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -27,6 +33,8 @@ public class BaseInitialization {
 	public static WebDriver driver;
 	public static String Userdir = System.getProperty("user.dir")+"\\Results\\";
 	public static String PathForDriver=System.getProperty("user.dir")+"\\src\\main\\java\\Jars\\";
+	public static ArrayList<String> names = new ArrayList<String>();
+	public static String strtestname="";
 
 	// added by Ramesh Nalla on 30-sep-2019 intializing driver
 	public WebDriver InitializeDriver() {
@@ -37,6 +45,7 @@ public class BaseInitialization {
 		case "chrome":
 			System.setProperty("webdriver.chrome.driver", PathForDriver+"chromedriver.exe");
 			driver = new ChromeDriver();
+			driver.manage().window().maximize();
 			break;
 		case "firefox":
 			System.setProperty("webdriver.gecko.driver", PathForDriver+"geckodriver.exe");
@@ -53,14 +62,23 @@ public class BaseInitialization {
 	}
 	
 	int i=0;
+	public void LoadProp() throws IOException {
+		Properties Prop = new Properties();
+		FileInputStream files = new FileInputStream(System.getProperty("user.dir")+"\\src\\main\\java\\resources\\Prop.properties");
+		Prop.load(files);
+		strtestname = Prop.getProperty("testname");
+	}
+	
 	public void Screenshot(String name) throws IOException {
 		i=i+1;
 		Properties Prop = new Properties();
 		FileInputStream files = new FileInputStream(System.getProperty("user.dir")+"\\src\\main\\java\\resources\\Prop.properties");
 		Prop.load(files);
-		String str = Prop.getProperty("testname");
+		String strtestname = Prop.getProperty("testname");
 		File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-		FileUtils.copyFile(src, new File(System.getProperty("user.dir")+"\\Results\\"+str+"\\"+i+". "+name+".png"));
+		String finalname = i+". "+name;
+		FileUtils.copyFile(src, new File(System.getProperty("user.dir")+"\\Results\\"+strtestname+"\\"+finalname+".png"));
+		names.add(finalname);
 	}
 
 	public static void MakeResultsFolderForTest(String TestName) throws IOException, InterruptedException {
@@ -69,22 +87,46 @@ public class BaseInitialization {
 		File origsrc = src;
 		if (!src.isDirectory()) {
 			src.mkdir();
-			System.out.println("completed with making folder");
+			System.out.println("Creating a new Results folder");
 		} else {
 			DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd-MM-yyyy_HH-mm-ss");
 			String now = dtf.format(LocalDateTime.now());
 			String strwithdate = TestName + "_" + now;
 			File newDir = new File(src.getParent() + "\\" + strwithdate);
 			src.renameTo(newDir);
-			System.out.println(newDir);
 			File destDir = new File(src.getParent() + "\\" + "Archive\\" + strwithdate);
-			System.out.println(destDir);
 			FileUtils.moveDirectory(newDir, destDir);
-			System.out.println("Completed with else block");
+			System.out.println("Moved previous folder to Archieve and created freshly");
 
 			origsrc.mkdir();
 
 		}
+	}
+	public void EndScreensDoc() {
+		try {
+
+            XWPFDocument docx = new XWPFDocument();
+            XWPFRun run = docx.createParagraph().createRun();
+            FileOutputStream out = new FileOutputStream("C:\\Users\\UX015235\\eclipse-workspace\\SharedFrameWork\\Results\\TesterLogin\\Res.doc");
+
+            for (String str : names) {
+                docx.createParagraph();
+                run.addBreak();
+                run.setText(str);
+                InputStream pic = new FileInputStream(Userdir+strtestname+"\\"+str+".png");
+                run.addPicture(pic , XWPFDocument.PICTURE_TYPE_PNG, "", Units.toEMU(475), Units.toEMU(280));
+            }
+
+            docx.write(out);
+            out.flush();
+            out.close();
+            docx.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("exeception block for EndScreensDoc() in BaseInti");
+        }
+		
 	}
 
 }
--- src/test/java/Hybrid/TesterLogin.java
+++ src/test/java/Hybrid/TesterLogin.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
@@ -20,9 +21,11 @@ public class TesterLogin extends BaseInitialization{
 	File FolderPath=new File(System.getProperty("user.dir")); 
 	
 	
+	
 	@BeforeTest
 	public void intialize() throws IOException, InterruptedException {
 		driver = InitializeDriver();
+		LoadProp();
 		MakeResultsFolderForTest(className);
 		Properties Prop = new Properties();
 		FileOutputStream files = new FileOutputStream(FolderPath+"\\src\\main\\java\\resources\\Prop.properties");
@@ -38,14 +41,14 @@ public class TesterLogin extends BaseInitialization{
 		Thread.sleep(3000);
 		Screenshot("Login Screen");
 		UltiLogin ul = new UltiLogin(driver);
-		ul.EnterUserID().sendKeys("");
+		ul.EnterUserID().sendKeys("1023170");
 		Thread.sleep(3000);
 		Screenshot("User ID enetered");
 		ul.ClickProcedBttn().click();
 		Thread.sleep(3000);
 		Screenshot("Selected Password");
 		ul.PassBttn().click();
-		ul.Passfield().sendKeys("");
+		ul.Passfield().sendKeys("Ulti@sep01");
 		Thread.sleep(3000);
 		Screenshot("Entered Password");
 		ul.LoginBttn().click();
@@ -56,6 +59,7 @@ public class TesterLogin extends BaseInitialization{
 	}
 	@AfterTest
 	public void closeAll() {
+		EndScreensDoc();
 		driver.quit();
 	}
 
